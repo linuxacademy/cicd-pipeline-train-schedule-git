c9f6027d83c776a7a7383d5305326ac3f8293787
selvasm <43107299+selvasm@users.noreply.github.com>
2018-09-09 22:40:06 +0200

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+/*Editing a new comment*/
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
