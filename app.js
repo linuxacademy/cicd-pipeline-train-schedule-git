2a21e48ff6890b45108ce5ffe33b56b79f679260
byronjohnsonk <byronjohnsonk@gmail.com>
2018-12-23 20:46:35 -0500

    added an explamation point

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// error handler!
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
