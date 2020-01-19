a8d4109f024dcc90361258b1415c4db1fda6f3ba
mshadaab <mshadaab12@gmail.com>
2020-01-19 15:08:37 -0500

    made a change

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', indexRouter);
 app.use('/trains', trainsRouter);
 
