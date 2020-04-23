2c58ae791a17943315a7f0ae19c049074bb48b17
Sachin <sachinyadavg@gmail.com>
2020-04-23 22:20:21 +0000

    added comment to app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var trainsRouter = require('./routes/trains');
 
 var app = express();
 
-// view engine setup
+// view engine setup - added comment for testing
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
